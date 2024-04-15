e50ce8efc9b75087732cb10cebe329b1ed5df38f
JunWoo Lee <junwoo1357@gmail.com>
2024-04-15 19:53:01 +0900

    numpytest

---
+++ chapter_1/numpytest.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+x = np.array([1.0, 2.0, 3.0])
+print(x)
+
+# 산술 연산
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(y / x)
