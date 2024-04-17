6353ceeabfa3ab5d29d9e27e669eabd0b6534ff4
hizen <hizen@finehc.ai>
2024-04-17 15:43:21 +0900

    3단원 최종

--- chapter_3.py
+++ chapter_3.py
@@ -103,25 +103,84 @@ import matplotlib.pylab as plt
 #################################################################
 # MNIST
 import sys, os
+
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
+import pickle
 
 
-def img_show(img):
-    pil_img = Image.fromarray(np.uint8(img))
-    pil_img.show()
+#
+#
+# def img_show(img):
+#     pil_img = Image.fromarray(np.uint8(img))
+#     pil_img.show()
+#
+# (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+#
+# img = x_train[0]
+# print("x_train : ", x_train)
+# print("t_train : ", t_train)
+# print("len x_train : ", len(x_train))
+# print("len t_train : ", len(t_train))
+# label = t_train[0]
+# print(label)  # 5
+#
+# print(img.shape)  # (784,)
+# img = img.reshape(28, 28)  # 형상을 원래 이미지의 크기로 변형
+# print(img.shape)  # (28, 28)
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+# img_show(img)
+#################################################################
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-img = x_train[0]
-label = t_train[0]
-print(label)  # 5
 
-print(img.shape)  # (784,)
-img = img.reshape(28, 28)  # 형상을 원래 이미지의 크기로 변형
-print(img.shape)  # (28, 28)
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
 
-img_show(img)
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+    return y
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+
+    p = np.argmax(y_batch, axis=1) # axis=1 : 최대값 인덱스 찾기.
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print(accuracy_cnt)
+print(len(x))
+print("Accuracy:", str(float(accuracy_cnt)/len(x)))
 #################################################################
